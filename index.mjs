a21b368aba6b0000399944a9b2cd7291b51f2240
Ze Si Dong (Eric) Li <eric.li@faire.com>
2023-01-27 09:34:14 -0500

    generate log instead of file

--- index.mjs
+++ index.mjs
@@ -18,12 +18,12 @@ async function getLogs(apiInstance, params) {
     let nextPage = null;
     let n = 0;
     do {
-        console.log(`Requesting page ${++n} ${nextPage ? `with cursor ${nextPage} ` : ``}`);
+        console.warn(`Requesting page ${++n} ${nextPage ? `with cursor ${nextPage} ` : ``}`);
         const query = nextPage ? { ...params, pageCursor: nextPage } : params;
         const result = await apiInstance.listLogsGet(query);
         data.push(...result.data);
         nextPage = result?.meta?.page?.after;
-        console.log(`${result.data.length} results (${data.length} total)`);
+        console.warn(`${result.data.length} results (${data.length} total)`);
     } while (nextPage);
 
     return data;
@@ -42,24 +42,44 @@ const initialParams = {
 };
 
 if (!initialParams.filterQuery) {
-    console.log(chalk.red("Error: No query supplied, use --query"));
+    console.warn(chalk.red("Error: No query supplied, use --query"));
     process.exit();
 }
 
-console.log(chalk.cyan("Downloading logs:\n" + JSON.stringify(initialParams, null, 2) + "\n"));
+console.warn(chalk.cyan("Downloading logs:\n" + JSON.stringify(initialParams, null, 2) + "\n"));
 
 (async function () {
     let data;
     try {
         data = await getLogs(apiInstance, initialParams);
     } catch (e) {
-        console.log(chalk.red(e.message));
+        console.warn(chalk.red(e.message));
         process.exit(1);
     }
 
     const outputFile = argv.output ?? "results.json";
-    console.log(chalk.cyan(`\nWriting ${data.length} logs to ${outputFile}`));
-    fs.writeFileSync(outputFile, JSON.stringify(data, null, 2));
+    console.warn(chalk.cyan(`\nWriting ${data.length} logs to ${outputFile}`));
+    // console.warn(JSON.stringify(data, null, 2));
+    for (let datum of data) {
+        // console.warn(chalk.cyan(`${data[0].id}`));
+        // console.warn(chalk.cyan(`${data[0].type}`));
+        // console.warn(chalk.cyan(`${data[0].attributes}`));
+        // console.warn(chalk.cyan(`${data[0].attributes.attributes.op}`));
+        // console.warn(chalk.cyan(`${data[0].attributes.attributes.job_message}`));
+        //fs.writeFileSync(outputFile, JSON.stringify(data, null, 2));
 
-    console.log(chalk.green("Done!"));
+        const attribute = datum.attributes
+        // const attributes = attribute?.attributes
+        // console.warn("INSERT INTO etl._temp_investigate_FWM_dd_logs ( log_id, log_level, logged_at, message, op_token, job_name, job_description)" +
+        // ` VALUES ( '${datum.id}', '${attributes?.level}', '${attributes?.timestamp}', '${attribute?.message}', '${attributes?.op}', '${attributes?.job}', '${attributes?.job_message?.description}');`)
+
+        const attributes = attribute.attributes
+        console.log("INSERT INTO etl._temp_investigate_FWM_dd_logs ( log_id, log_level, logged_at, message, op_token, job_name, job_description)" +
+        ` VALUES ( '${datum.id}', '${attributes.level}', '${attributes.timestamp}', '${attribute.message}', '${attributes.op}', '${attributes.job}', '${attributes.job_message.description}');`)
+        //console.warn(JSON.stringify(datum, null, 2));
+        
+    }
+    
+    console.warn(chalk.green("Done!"));
 })();
+//node index.mjs --query 'env:"production" @queue:"outgoing-notifications-sender" service:"backend-worker"' --from 2023-01-23T00:00:00.000Z --to 2023-01-23T12:00:00.000Z > output.sql
